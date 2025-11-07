45fec2dc857f54753eb3c6717d09414e29c8ed01
marco bubola <marcobubola@hotmail.com>
2025-11-07 19:20:01 -0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const PokedexApp());
+}
+
+class PokedexApp extends StatelessWidget {
+  const PokedexApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Pokédex da FATEC',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ), // ThemeData
+      home: const HomeScreen(),
+    ); // MaterialApp
+  }
+}
